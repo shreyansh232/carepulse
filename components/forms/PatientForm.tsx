7d9826e6d6fcb77a1f0540e6ce1e7faab35cf303
shreyansh-web <shreyansh487@gmail.com>
2024-08-24 21:20:29 +0530

    feat: add appwrite

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = "input",
@@ -46,9 +47,10 @@ const PatientForm = () => {
         email,
         phone,
       };
-    //   const user = await createUser(userData);
-    //   if (user) {
-    //     router.push(`/patients/${user.$id}/register`);
+      const user = await createUser(userData);
+      if (user) {
+        router.push(`/patients/${user.$id}/register`);
+      }
     } catch (error) {
       console.log(error);
     }
